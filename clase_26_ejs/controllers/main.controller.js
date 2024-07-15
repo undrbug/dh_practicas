11b481cc26ace5cf9575c3bf9c8dc675b8ff84d0
hGauna <undrbug@gmail.com>
2024-07-14 21:46:02 -0300

    rutas compeletas

--- clase_26_ejs/controllers/main.controller.js
+++ clase_26_ejs/controllers/main.controller.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const data = require('../model/data.json');
+const aboutData = require('../model/about.json');
 
 
 const app = express();
 
 const mainController = {
     home: (req, res) => {
-        res.render('home.ejs');
+        res.render('index.ejs');
     },
-    menu: (req, res) => {
+    detalle: (req, res) => {
         const id = req.params.id;
         const dataquery = data.filter((item) => item.id == id);
-        res.render('menu.ejs', {dataquery: dataquery});
+        //una forma de hacerlo
+        const menu = {
+            'id': dataquery[0].id,
+            'nombre': dataquery[0].nombre,
+            'descripcion': dataquery[0].descripcion,
+            'imagen': dataquery[0].imagen,
+            'precio': dataquery[0].precio
+        }
+        //otra forma de hacer lo mismo
+        //const menu2 = dataquery[0];
+        res.render('home.ejs', {menu: menu});
     },
+    about: (req, res) => {
+        const random = Math.floor(Math.random() * aboutData.length);
+        const about = aboutData[random];
+        res.render('about.ejs', {about: about});
+    }
 
 };
 
--- clase_26_ejs/routes/main.route.js
+++ clase_26_ejs/routes/main.route.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const mainController = require('../controllers/main.controller.js');
 
 router.get('/', mainController.home);
-router.get('/menu/:id', mainController.menu);
+router.get('/detalle/:id', mainController.detalle);
+router.get('/about', mainController.about);
 
 
 module.exports = router;
