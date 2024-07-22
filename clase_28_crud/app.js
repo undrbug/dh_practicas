2932448495a8f8dd395e2f8f2fa591947bb55674
Saúl Hernán Gauna <hgauna@iprodha.misiones.gov.ar>
2024-07-22 19:23:04 -0300

    se agrego el middleware para error 404

--- clase_28_crud/app.js
+++ clase_28_crud/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const userRouter = require('./user.router');
+const path = require('path');
+const ejs = require('ejs');
+const fs = require('fs');
+
+const users = {
+    id: "1",
+    nombre: "hgauna",
+    email: "pepe@argento.com"
+};
+
+//dirname guarda la ruta actual. La ruta desde donde se esta ejecutando la sentencia
+// const ruta = path.join(__dirname, 'users/');
+// console.log(ruta);
+// fs.writeFileSync(`users.json`, "pepe: otro" );
 
 const app = express();
 
@@ -9,12 +23,18 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.static('public'));
 
 const PORT = 3000 || process.env.PORT;
 
 app.use(methodOverride('_method'));
 
 app.use('/', userRouter)
+
+app.use((req, res, next) => {
+    res.status(404).render('Not-found.ejs');
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
