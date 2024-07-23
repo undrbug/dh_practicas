e28b67c85881e0ff3ac4bceb719b07b4778cca16
hGauna <undrbug@gmail.com>
2024-07-22 21:05:17 -0300

    add fs, modifique funciones para el crud con persistencia en arhivos Json

--- clase_28_crud/user.controller.js
+++ clase_28_crud/user.controller.js
@@ -1,19 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
-
-let usuario = {
-    id: null,
-    nombre: null,
-};
-
-const usuarioArray = []
+let usersList = fs.readFileSync('users.json', 'utf-8');
+let usuarioArray = JSON.parse(usersList);
 
 const userController = {
     crearUsuario: (req, res) => {
-        usuario.id = (usuarioArray.length + 1);
-        usuario.nombre = req.body.nombre;
-        usuarioArray.push(usuario)
-        res.send(usuario);
+        const userNew = {
+            "id": (usuarioArray.length + 1),
+            "nombre": req.body.nombre,
+            "apellido": req.body.apellido,
+            "email": req.body.email
+        }
+        usuarioArray.push(userNew)
+        res.send(usuarioArray);
+        fs.writeFileSync('users.json', JSON.stringify(usuarioArray));
     },
     listarUsuarios: (req, res) => {
         res.send(usuarioArray);
@@ -21,22 +22,38 @@ const userController = {
     actualizarUsuario: (req, res) => {
         const id = req.body.id;
         const usuario = usuarioArray.find(usuario => usuario.id == id);
+        if (!usuario) {
+            res.send('Usuario no encontrado');
+        }
+        const index = usuarioArray.indexOf(usuario);
+        usuarioArray.splice(index, 1);
         usuario.nombre = req.body.nombre;
-        res.render({usuario: usuario});
+        usuario.apellido = req.body.apellido;
+        usuario.email = req.body.email
+        usuarioArray.push(usuario);
+        fs.writeFileSync('users.json', JSON.stringify(usuarioArray));
+        res.send(usuario);
 
 
     },
     borrarUsuario: (req, res) => {
         const id = req.params.id;
         const usuario = usuarioArray.find(usuario => usuario.id == id);
+        if (!usuario) {
+            res.send('Usuario no encontrado');
+        }
         const index = usuarioArray.indexOf(usuario);
         usuarioArray.splice(index, 1);
-        res.render({usuario: usuario});
+        fs.writeFileSync('users.json', JSON.stringify(usuarioArray));
+        res.send(usuario);
     },
     buscarUsuario: (req, res) => {
         const id = req.params.id;
         const usuario = usuarioArray.find(usuario => usuario.id == id);
-        res.render({usuario: usuario});
+        if (!usuario) {
+            res.send('Usuario no encontrado');
+        }
+        res.send({usuario: usuario});
     }
 
 }
